b393dbb50e22fac15fb7cec7d7b14c331392acef
Robin Lloyd <angelssindesign@gmail.com>
2015-08-03 13:40:39 +0100

    remove livereload code (not needed) and test text

--- functions.php
+++ functions.php
@@ -38,16 +38,6 @@ function threesugars_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'threesugars_styles' );
 
-function livereload_script() {
-    wp_register_script('livereload', 'http://localhost:35729/livereload.js?snipver=1', null, false, true);
-    wp_enqueue_script('livereload');
-}
-
-// if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
-    // echo "this works";
-    add_action( 'wp_enqueue_scripts', 'livereload_script' );
-// }
-
 
 /*-------------------------------------------------------------*/
 /* Register Menus
